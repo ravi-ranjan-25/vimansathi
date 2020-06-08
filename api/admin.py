29bed4192c8faa3866672b09736cd9e1e7caeaab
Your Name <raviranjanv25@gmail.com>
2020-06-09 03:17:06 +0530

    some desperate code changes to try fix heroku

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from api.models import userdetails,Product,wallet,order,hotel,storerestro
+from api.models import userdetails,Product,wallet,order,hotel,storerestro,Complain,Tax
 
 # Register your models here.
 
@@ -8,4 +8,6 @@ admin.site.register(Product)
 admin.site.register(wallet)
 admin.site.register(order)
 admin.site.register(hotel)
+admin.site.register(Tax)
+admin.site.register(Complain)
 admin.site.register(storerestro)
