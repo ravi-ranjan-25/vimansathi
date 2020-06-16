e157d6493033488f11a7867c1960e3a14c1ccee7
Your Name <raviranjanv25@gmail.com>
2020-06-16 12:00:20 +0530

    a

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from api.models import userdetails,Product,wallet,order,hotel,storerestro,Complain,Tax,cat
+from api.models import userdetails,Product,wallet,order,hotel,storerestro,Complain,Tax
 
 # Register your models here.
 
