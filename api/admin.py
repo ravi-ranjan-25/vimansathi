974b7f850027c475ae40e62f83df54f5b9a4681e
Your Name <raviranjanv25@gmail.com>
2020-07-10 18:22:30 +0530

    ini

--- api/admin.py
+++ api/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from api.models import userdetails,Product,wallet,order,hotel,storerestro,Complain,Tax,cat,airline,routes,days
+from api.models import userdetails,Product,wallet,order,hotel,storerestro,Complain,Tax,cat,airline,routes,days,airport
 
 # Register your models here.
 
@@ -15,3 +15,4 @@ admin.site.register(cat)
 admin.site.register(airline)
 admin.site.register(routes)
 admin.site.register(days)
+admin.site.register(airport)
