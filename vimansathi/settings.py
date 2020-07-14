a4440d008a8bc31da3ef3f5c36ce9d98bb3e9eb8
Your Name <raviranjanv25@gmail.com>
2020-07-14 13:45:32 +0530

    ss

--- vimansathi/settings.py
+++ vimansathi/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '*1w^vq14rmjcso9)h!w))^70g$l1s$yu!xz6o(*ri%rqrej%e3'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://vimansathi.herokuapp.com/','django-env.eba-tdudpixp.ap-south-1.elasticbeanstalk.com']
+ALLOWED_HOSTS = ['https://vimansathi.herokuapp.com/','django-env.eba-rmcmhham.ap-south-1.elasticbeanstalk.com']
 
 
 # Application definition
