e3f3ddbe4fa25d8acd5c8af75260e193b4f4f71b
Your Name <raviranjanv25@gmail.com>
2020-07-14 15:35:28 +0530

    ss

--- vimansathi/settings.py
+++ vimansathi/settings.py
@@ -134,8 +134,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
-STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
-STATIC_URL = 'static'
+STATIC_ROOT = STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+STATIC_URL = '/static/'
 TEMPLATE_DIRS = (
                     os.path.join(os.path.dirname(__file__),'templates'),
 )
